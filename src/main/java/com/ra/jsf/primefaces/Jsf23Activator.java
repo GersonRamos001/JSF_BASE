d9d11de9b732e083ee64e0b4f8b7a57693b238e8
GersonRamos001 <ramosargueta@gmail.com>
2023-10-30 20:46:51 -0600

    Initial Commit

---
+++ src/main/java/com/ra/jsf/primefaces/Jsf23Activator.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.ra.jsf.primefaces;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.faces.annotation.FacesConfig;
+
+@ApplicationScoped
+@FacesConfig(version = FacesConfig.Version.JSF_2_3)
+public class Jsf23Activator {
+}
